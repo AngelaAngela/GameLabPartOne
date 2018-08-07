529e30e95cb64cc54c70bfb3abc4fe7973fb152b
angie <angieangerilli@gmail.com>
2018-08-06 20:41:58 -0400

    GameLabPartOne

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+
+let person = prompt("Do you want to play?");
+if (person === "yes"){
+let user = prompt("What is your name?");
+
+
+
+
 let userHealthPoints = 40;
 let grantHealthPoints = 10;
 let wins = 0;
 
+
+
 while (wins < 3 && grantHealthPoints >= 0){
 
-grantHealthPoints -= Math.floor((Math.random() *1)+2);
-userHealthPoints -= Math.floor(Math.random() *1)+2;
-  console.log( `grants ${grantHealthPoints}`);
-  console.log(`user ${userHealthPoints}`)
+grantHealthPoints -= Math.floor(Math.random() *2)+1;
+userHealthPoints -= Math.floor(Math.random() *2)+1;
+ 
+  console.log( `Grant the Almighty has ${grantHealthPoints} left.`);
+  
+  console.log(`${user} has ${userHealthPoints} left.`)
+  
   if (grantHealthPoints === 0){
 wins++;
 grantHealthPoints=10;
-console.log("user wins")
+console.log("User wins")
 }
 }
-
+}
 
 
