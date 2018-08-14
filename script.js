3efdb98736f7bd1dd74c935ef96d3057ffd5fd03
angie <angieangerilli@gmail.com>
2018-08-13 20:24:27 -0400

    GameLabPartOne

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-
-let person = prompt("Do you want to play?");
-if (person === "yes"){
-let user = prompt("What is your name?");
+"use strict";{
+function startGame(){
 
 
+	let person = prompt("Do you want to play?");
+	if (person === "yes") {
+
+		let user = prompt("What is your name?");
+		let userHealthPoints = 40;
+		let grantHealthPoints = 10;
+		let wins = 0;
+		startCombat();
+
+		function startCombat (){
 
 
-let userHealthPoints = 40;
-let grantHealthPoints = 10;
-let wins = 0;
+			while (wins < 3 && userHealthPoints > 0){
 
-
-
-while (wins < 3 && grantHealthPoints >= 0){
-
-grantHealthPoints -= Math.floor(Math.random() *2)+1;
-userHealthPoints -= Math.floor(Math.random() *2)+1;
- 
-  console.log( `Grant the Almighty has ${grantHealthPoints} left.`);
-  
-  console.log(`${user} has ${userHealthPoints} left.`)
-  
-  if (grantHealthPoints === 0){
-wins++;
-grantHealthPoints=10;
-console.log("User wins")
+				const action = prompt("attack or quit? (enter 'a' or 'q' for your choice)")
+           if (action !== "a") {
+               console.log ("Thank you for playing today!");
+               break;
+            }
+				getDamage();
+			  	console.log( `Grant the Almighty has ${grantHealthPoints} left.`);
+			  
+			  	console.log(`${user} has ${userHealthPoints} left.`)
+			  
+			  	if (grantHealthPoints <= 0){
+					wins++;
+					grantHealthPoints=10;
+					console.log("User wins")
+				}
+			}
+		}
+		function getDamage(){
+			grantHealthPoints -= Math.floor(Math.random() *5)+1;
+			userHealthPoints -= Math.floor(Math.random() *5)+1;
 }
-}
-}
+
+	}
 
 
+
+}
+
+startGame();
+}
